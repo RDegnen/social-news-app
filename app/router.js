e89d52560e8295715556445090a0d24018600d2e
Ross <rdegnen@gmail.com>
2016-07-11 22:14:48 -0400

    New post page has inputs
    Decided to use ember-cli-showdown as a markdown editor for posts. Have simple inputs but no actions to save them yet

--- app/router.js
+++ app/router.js
@@ -8,6 +8,7 @@ const Router = Ember.Router.extend({
 Router.map(function () {
   this.route('posts', { path: '/'});
   this.route('post', { path: '/posts/:post_id'});
+  this.route('new-post', { path: 'posts/submit'});
 });
 
 export default Router;
